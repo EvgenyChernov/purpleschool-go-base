eadfa4ae0500198810ab67c842ffa4fc7684b3b9
evgeny.chernov <EvgenyCher89@gmail.com>
2025-12-06 17:50:33 +0300

    Пример переменных

--- go-demo/main.go
+++ go-demo/main.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main() {
+	const IMTPower float64 = 2
 	userHeight := 1.8
 	var userKg float64
 	userKg = 100
-	IMT := userKg / math.Pow(userHeight, 2)
+	IMT := userKg / math.Pow(userHeight, IMTPower)
 	fmt.Print(IMT)
 }
