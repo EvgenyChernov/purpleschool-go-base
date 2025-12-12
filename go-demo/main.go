2a4cf3c245cc170a5b7762851ec62625c353076d
evgeny.chernov <EvgenyCher89@gmail.com>
2025-12-12 17:57:46 +0300

    Использование функций

--- go-demo/main.go
+++ go-demo/main.go
@@ -6,20 +6,33 @@ import (
 )
 
 func main() {
-	const IMTPower float64 = 2
-	var userKg float64
-	var userHeight float64
-	fmt.Println("___Калькулятор индекса массы тела___")
-	fmt.Print("Введите свой рост в сантиметрах: ")
-	fmt.Scan(&userHeight)
-	fmt.Print("Введите свой вес в кг: ")
-	fmt.Scan(&userKg)
-	IMT := userKg / math.Pow(userHeight/100, IMTPower)
 
+	fmt.Println("___Калькулятор индекса массы тела___")
+	userHeight, userKg := getUserInput()
+	IMT := calculateIMT(userHeight, userKg)
 	outputResult(IMT)
 }
 
 func outputResult(imt float64) {
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
 	fmt.Print(result)
+}
+
+func calculateIMT(userHeight float64, userKg float64) float64 {
+	const IMTPower float64 = 2
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	return IMT
+}
+
+func getUserInput() (float64, float64) {
+	var userKg float64
+	var userHeight float64
+
+	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+
+	fmt.Print("Введите свой вес в кг: ")
+	fmt.Scan(&userKg)
+
+	return userHeight, userKg
 }
