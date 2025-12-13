15c411d02ab744c819d226280b7c006e7056404f
evgeny.chernov <EvgenyCher89@gmail.com>
2025-12-13 19:46:49 +0300

    Сделал консольную программу которая принимает строку проверяет на символы и добававляет число в массив чисел после считает сумму

--- go-demo-2/main.go
+++ go-demo-2/main.go
@@ -1,14 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	transactions := [3]int{5, 10, -7}
-	banks := [2]string{"Тинькофф", "Альфа"}
+	transactions := []int{}
+	var inputData string
 
-	fmt.Println(transactions)
-	fmt.Println(banks)
+	for {
+		fmt.Println("Введите тарнзакцию или 'd' для подсчета суммы, спасибо.")
+		fmt.Scan(&inputData)
 
-	patiral := transactions[1:2]
-	fmt.Println(patiral)
+		if isNumber(inputData) {
+			numberTransition, _ := strconv.Atoi(inputData)
+			transactions = append(transactions, numberTransition)
+			fmt.Println(transactions)
+		} else if inputData == "d" {
+			fmt.Println("буква Д")
+			sum := calculateAllTransition(transactions)
+			fmt.Println("Сумма транзакций равна ", sum)
+			break
+		} else {
+			break
+		}
+	}
+}
+
+func calculateAllTransition(arr []int) int {
+	sum := 0
+	for _, value := range arr {
+		sum += value
+	}
+	return sum
+}
+
+func isNumber(s string) bool {
+	_, err := strconv.Atoi(s)
+	return err == nil
 }
