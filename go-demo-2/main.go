61fec694348488a27c1622d78a9851810ac47337
evgeny.chernov <EvgenyCher89@gmail.com>
2025-12-13 13:18:59 +0300

    рассмотр слайса

--- go-demo-2/main.go
+++ go-demo-2/main.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	transactions := [3]int{5, 10, -7}
 	banks := [2]string{"Тинькофф", "Альфа"}
+
+	fmt.Println(transactions)
+	fmt.Println(banks)
+
+	patiral := transactions[1:2]
+	fmt.Println(patiral)
 }
