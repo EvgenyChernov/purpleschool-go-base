b7248350bf718c5a81e29ea67a4ef11325ab551a
evgeny.chernov <EvgenyCher89@gmail.com>
2025-12-18 16:17:19 +0300

    Импорт эспорт пакетов и методов

---
+++ go-demo-4/account/account.go
@@ -0,0 +1,59 @@
+package account
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"net/url"
+	"time"
+)
+
+type Account struct {
+	login    string
+	password string
+	url      string
+}
+
+type accountWithTimeStamp struct {
+	createdAt time.Time
+	updatedAt time.Time
+	Account
+}
+
+func (acc Account) OutputPassword() {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *Account) GeneratPassword() {
+	var lenghtPassword int = 10
+	var localPassword string
+	for i := 0; i < lenghtPassword; i++ {
+		localPassword += randomLetter()
+	}
+	acc.password = localPassword
+}
+
+func NewAccountWithTimeStam(login, password, urlString string) (*accountWithTimeStamp, error) {
+	if login == "" || password == "" || urlString == "" {
+		return nil, errors.New("invalid input")
+	}
+
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+	return &accountWithTimeStamp{
+		Account: Account{
+			url:      urlString,
+			password: password,
+			login:    login,
+		},
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+	}, nil
+}
+
+func randomLetter() string {
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	return string(letters[rand.Intn(len(letters))])
+}
--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -1,73 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math/rand"
-	"net/url"
-	"time"
+	"demo/password/account"
 )
-
-type account struct {
-	login    string
-	password string
-	url      string
-}
-
-type accountWithTimeStamp struct {
-	createdAt time.Time
-	updatedAt time.Time
-	account
-}
-
-func (acc account) outputPassword() {
-	fmt.Println(acc.login, acc.password, acc.url)
-}
-
-func (acc *account) generatPassword() {
-	var lenghtPassword int = 10
-	var localPassword string
-	for i := 0; i < lenghtPassword; i++ {
-		localPassword += randomLetter()
-	}
-	acc.password = localPassword
-}
-
-func newAccountWithTimeStam(login, password, urlString string) (*accountWithTimeStamp, error) {
-	if login == "" || password == "" || urlString == "" {
-		return nil, errors.New("invalid input")
-	}
-
-	_, err := url.ParseRequestURI(urlString)
-	if err != nil {
-		return nil, errors.New("INVALID_URL")
-	}
-	return &accountWithTimeStamp{
-		account: account{
-			url:      urlString,
-			password: password,
-			login:    login,
-		},
-		createdAt: time.Now(),
-		updatedAt: time.Now(),
-	}, nil
-}
-
-// func newAccount(login, password, urlString string) (*account, error) {
-// 	if login == "" || password == "" || urlString == "" {
-// 		return nil, errors.New("invalid input")
-// 	}
-
-// 	_, err := url.ParseRequestURI(urlString)
-// 	if err != nil {
-// 		return nil, errors.New("INVALID_URL")
-// 	}
-// 	return &account{
-// 		url:      urlString,
-// 		password: password,
-// 		login:    login,
-// 	}, nil
-// }
 
 func main() {
 
@@ -75,14 +11,14 @@ func main() {
 	password := promptData("Введите пароль")
 	url := promptData("Введите URL")
 
-	account1, err := newAccountWithTimeStam(login, password, url)
+	account1, err := account.NewAccountWithTimeStam(login, password, url)
 	if err != nil {
 		fmt.Println("Неверный формат URL")
 		return
 	}
-	account1.outputPassword()
-	account1.generatPassword()
-	account1.outputPassword()
+	account1.OutputPassword()
+	account1.GeneratPassword()
+	account1.OutputPassword()
 }
 
 func promptData(prompt string) string {
@@ -90,9 +26,4 @@ func promptData(prompt string) string {
 	var res string
 	fmt.Scan(&res)
 	return res
-}
-
-func randomLetter() string {
-	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	return string(letters[rand.Intn(len(letters))])
 }
