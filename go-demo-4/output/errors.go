cbb2488dab2dcc5ef175335affec9730d3b2cdc9
evgeny.chernov <EvgenyCher89@gmail.com>
2025-12-23 16:36:25 +0300

    получение типа

--- go-demo-4/output/errors.go
+++ go-demo-4/output/errors.go
@@ -3,12 +3,27 @@ package output
 import "fmt"
 
 func PrintError(value any) {
+	value, ok := value.(int)
+	if ok {
+		fmt.Println("int")
+		return
+	}
+	stringValue, ok := value.(string)
+	if ok {
+		fmt.Println("string")
+		return
+	}
+	errorValue, ok := value.(error)
+	if ok {
+		fmt.Println(errorValue.Error())
+		return
+	}
 
 	switch value.(type) {
 	case int:
-		fmt.Println("int")
+		fmt.Println(value)
 	case string:
-		fmt.Println("string")
+		fmt.Println(stringValue)
 	default:
 		fmt.Println("unknown")
 	}
