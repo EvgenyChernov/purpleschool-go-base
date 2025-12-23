49cee8bc86c7995b11d35fe787e322bf43c435c6
evgeny.chernov <EvgenyCher89@gmail.com>
2025-12-23 16:31:20 +0300

    switch case для типов данных

--- go-demo-4/output/errors.go
+++ go-demo-4/output/errors.go
@@ -3,5 +3,14 @@ package output
 import "fmt"
 
 func PrintError(value any) {
+
+	switch value.(type) {
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
+	default:
+		fmt.Println("unknown")
+	}
 	fmt.Println(value)
 }
