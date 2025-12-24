fc79552e4971a12b44d1ea17905f55074e0829c8
evgeny.chernov <EvgenyCher89@gmail.com>
2025-12-24 22:25:16 +0300

    простой гет запрос по апи

--- go-demo-5/main.go
+++ go-demo-5/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"demo/app-http/geo"
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	city := flag.String("city", "", "Город пользователя")
+	// format := flag.Int("format", 1, "Формат вывода погоды")
+
+	flag.Parse()
+
+	geoData, err := geo.GetMyLocation(*city)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(geoData)
+
 }
